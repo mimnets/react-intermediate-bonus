d8dde84653282a946ced264fcc52a6346f2f06e0
MiMNets <mdmonirulislammonir@gmail.com>
2020-09-08 01:22:26 +0600

    learn how to call multiple objects elements from api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 function App() {
   const [likeColor, setLikeColor] = useState(''); 
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+    // singleUser
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
+
+    // randomUser
+    fetch('https://randomuser.me/api')
+    .then(res => res.json())
+    .then(data => setRandomUser(data.results[0]))
+  },[])
   const handlLike = () => {
     const color = likeColor ? '' : 'primary';
     setLikeColor(color);
@@ -11,8 +29,18 @@ function App() {
   return (
     <div className="App">
       <ThumbUpAltIcon onClick={handlLike} color={likeColor}></ThumbUpAltIcon>
+      <h2>Random Gender: {randomUser.gender}</h2>
+      <h2>Random First Name : {randomUser.name && randomUser.name.first}</h2> {/* A methode for replaing the undefined value */}
+      <h2>Random Last Name : {randomUser.name?.last}</h2> {/* Another methode for replaing the undefined value */}
+
+      
+      <h1>{singleUser.name}</h1>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
     </div>
   );
 }
 
 export default App;
+
